f7367d9b34a5e7ae5dfa24137ccc4cffdc69ae63
your Name <mohamedabdulayubm@gmail.com>
2022-06-30 15:54:13 +0530

    PeakPop

--- StackandQueue/Program.cs
+++ StackandQueue/Program.cs
@@ -3,10 +3,24 @@
 Console.WriteLine("Welcome to Stack and Queue");
 
 
-
+//Stack and Pop
 Stack stack = new Stack();
 stack.StackPush(70);
 stack.StackPush(30);
 stack.StackPush(56);
-stack.DisplayStack();
+stack.Display();
+stack.Pop();
+stack.Display();
+
+Console.WriteLine("-----------------------------------------------");
+
+
+//Peak
+Stack stack1 = new Stack();
+stack1.StackPush(70);
+stack1.StackPush(30);
+stack1.StackPush(56);
+stack1.Peak();
+stack1.Display();
+
 
--- StackandQueue/Stack.cs
+++ StackandQueue/Stack.cs
@@ -16,7 +16,7 @@ namespace StackandQueue
             this.head = null;
         }
 
-        internal void StackPush(int value)
+        public void StackPush(int value)
         {
             Node newNode = new Node(value);
             if (head == null)
@@ -30,8 +30,42 @@ namespace StackandQueue
             head = newNode;
             Console.WriteLine("{0} Added to stack", value);
         }
+        public void Pop()
+        {
+            Node newNode = head;
+             if(head == null)
+            {
+               newNode.next=null;
+                Console.WriteLine("Empty , Nothing to pop");
+                return;
+            }
+            else
+            {
+                while (head != null)
+                {
+                    Console.WriteLine("Popped Item is {0}", head.data);
+                    head = head.next;
+                }
+            }
+        }
+        public void Peak()
+        {
+            
+            if (head == null)
+            {
+                Console.WriteLine("Empty");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("{0} is on the top ", this.head.data);
+               
+            }
 
-        public void DisplayStack()
+        }
+        
+
+        public void Display()
         {
             Node head = this.head;
             if (head == null)
@@ -44,6 +78,7 @@ namespace StackandQueue
                 Console.WriteLine(head.data + " ");
                 head = head.next;
             }
+            Console.WriteLine("-----------------------------------------------------------");
         }
     }
 }
