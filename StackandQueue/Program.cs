d960587d1af21c778cc75f3bc94974f59028a185
your Name <mohamedabdulayubm@gmail.com>
2022-06-30 14:01:30 +0530

    Created Stack list

--- StackandQueue/Program.cs
+++ StackandQueue/Program.cs
@@ -1,2 +1,12 @@
-﻿
+﻿using StackandQueue;
+
 Console.WriteLine("Welcome to Stack and Queue");
+
+
+
+Stack stack = new Stack();
+stack.StackPush(70);
+stack.StackPush(30);
+stack.StackPush(56);
+stack.DisplayStack();
+
